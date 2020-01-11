e673f885cc30e8b7e8c29e4c130efb6d750ed1d0
ShadyDorgham <46231776+ShadyDorgham@users.noreply.github.com>
2020-01-11 17:26:05 +0200

    Update Rounding.cs

--- SalesTaxes/Helpers/Rounding.cs
+++ SalesTaxes/Helpers/Rounding.cs
@@ -18,7 +18,7 @@ namespace SalesTaxes.Helpers
                fractionToBeRounded = .1M;
  
 
-            return unRoundedAmount + fractionToBeRounded + "fsdf";
+            return unRoundedAmount + fractionToBeRounded;
         }
 
 
